90f41fef66e5d7a59c2097dc10f3a145d6b0b8aa
Ouk Outdam <outdam.ouk103@gmail.com>
2026-08-07 22:36:18 +0900

    feat: add REPL input loop to main

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+
+		userInput := scanner.Text()
+		userInputSlice := cleanInput(userInput)
+		if len(userInputSlice) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", userInputSlice[0])
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error reading input:", err)
+		os.Exit(1)
+	}
 }
