2abc14dc086f908efc84643dcf68b1f55422ff4a
Ouk Outdam <outdam.ouk103@gmail.com>
2026-08-07 23:14:20 +0900

    feat: add help and exit commands with a command registry
    - introduce cliCommand struct and cliCommands map for name-based dispatch
    - wire REPL loop to lookup and invoke commands from the map using .callback

--- main.go
+++ main.go
@@ -3,8 +3,33 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"maps"
 	"os"
+	"slices"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var cliCommands map[string]cliCommand
+
+func init() {
+	cliCommands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -20,11 +45,34 @@ func main() {
 		if len(userInputSlice) == 0 {
 			continue
 		}
-		fmt.Printf("Your command was: %s\n", userInputSlice[0])
+		cmd, ok := cliCommands[userInputSlice[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		if err := cmd.callback(); err != nil {
+			fmt.Fprintln(os.Stderr, "error running command:", err)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "error reading input:", err)
 		os.Exit(1)
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+
+	for _, name := range slices.Sorted(maps.Keys(cliCommands)) {
+		fmt.Printf("%s: %s\n", name, cliCommands[name].description)
+	}
+	return nil
 }
